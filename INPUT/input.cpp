a9fbf367b5bc2981e41cac71e87a404e00b3c95c
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-06-05 10:21:33 +0530

    added new

---
+++ INPUT/input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter the number: ";
+    cin>>a;
+    int b;
+    cout<<"enter the number: ";
+    cin>>b;
+    cout<<a+b;
+
+}
