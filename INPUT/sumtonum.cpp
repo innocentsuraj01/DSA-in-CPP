a307738f937ecc21a274ad10f15bcca92c7dd065
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-06-05 14:47:05 +0530

    added a new file

---
+++ INPUT/sumtonum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x;
+    cout<<"Enter First Number : ";
+    cin>>x;
+    int y;
+    cout<<"Enter Second Number :";
+    cin>>y;
+    int Sum =x+y;
+    cout<<"Sum of two numbers is : " << Sum;
+}
