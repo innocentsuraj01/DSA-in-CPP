da9e4365e05bccfb599a3c885479b78251c6534f
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-06-05 15:09:43 +0530

    added a new file

---
+++ INPUT/operators.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter first number : ";
+    cin>>x;
+    int y;
+    cout<<"Enter second number : ";
+    cin>>y;
+    cout<<(x==y);
+}
