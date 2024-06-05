591dbc22810f309827cded344d2c3e7b1683747f
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-06-05 14:29:05 +0530

    added a new file

---
+++ INPUT/Square.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int x;
+    cout<<"Enter a Number : ";
+    cin>>x;
+    int y = x*x;
+    cout<<"Square of Number that you gave is : ";
+    cout<<y;
+}
