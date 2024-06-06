4bcb0a9244606ecd76f0ef69c18ef9423472b503
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-06-06 11:39:21 +0530

    added a new file

---
+++ INPUT/predictoutput.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=2,b=3,c,d;
+    float x,y;
+    c = a / b * b;
+    d = b / a * a;
+    x = a / b * b;
+    y = b / a * a;
+    cout<<c<<" "<<d<<" "<<x<<" "<<y;
+}
