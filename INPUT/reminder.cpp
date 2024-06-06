25f5fa61b02956deba2647251075dd40f287f73f
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-06-06 15:48:51 +0530

    added a new file

---
+++ INPUT/reminder.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter the first number : ";
+    cin>>a;
+    cout<<"Enter the second number : ";
+    cin>>b;
+    c = a/b;
+    cout<<"Reminder of two numbers is : "<<c;
+
+}
