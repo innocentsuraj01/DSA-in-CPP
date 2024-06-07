2e2ce4d66704e447c94b14d35cc663df6c171954
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-06-07 11:17:23 +0530

    added a new file

--- INPUT/modulusoperator.cpp
+++ INPUT/modulusoperator.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 int main(){
     int a,b;
-    cout<<"Enter first number : "<<endl<<"Enter second number : ";
-    cin>>a>>b;
+    cout<<"Enter first number : ";
+    cin>>a;
+    cout<<"Enter second number : ";
+    cin>>b;
     float modulus = a%b;
     cout<<"The modulus of two number is : "<<modulus;
 }
---
+++ INPUT/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+ cout<<"Enter first number : "<<endl<<"Enter second number : ";
