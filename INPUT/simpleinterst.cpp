fb5d9b52fd1905e073626eafe313b0ba87aca9fc
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-06-07 11:55:58 +0530

    added a new line

--- INPUT/simpleinterst.cpp
+++ INPUT/simpleinterst.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    float p,r,t;
+    float p,r,t,si;
     cout<<"Enter principle : ";
     cin>>p;
     cout<<"Enter rate : ";
     cin>>r;
     cout<<"Enter time : ";
     cin>>t;
-    float si = (p*r*t)/100;
+    si = (p*r*t)/100;
     cout<<"The simple interst is : "<<si;    
 }
