fbfd455c0b8760a640e078eb50acbd01d557b53b
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-06-09 10:59:39 +0530

    Added a new file

---
+++ INPUT/grademarks.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Your Markes : ";
+    cin>>n;
+    if (n>=91){
+        cout<<"Excellent"; 
+    }
+    else{
+        if(n>=81){
+          cout<<"Very Good";  
+        }
+        else{
+            if(n>=71){
+            cout<<"Good";
+            }
+            else{
+                if(n>=61){
+                cout<<"Can do better";
+                }
+                else{
+                    if(n>=51){
+                        cout<<"Average";
+                    }
+                    else{
+                        if(n>=41){
+                            cout<<"Below Average";
+                        }
+                        else{
+                            if(n>40){
+                                cout<<"Fail";
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
