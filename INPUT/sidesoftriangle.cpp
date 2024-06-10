aa7e555b08538212a448e6946f100ad41b6e12f0
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-06-10 13:29:13 +0530

    Added a new fiole

---
+++ INPUT/sidesoftriangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a,b,c;
+    cout<<"Enter the first side : ";
+    cin>>a;
+    cout<<"Enter the second side : ";
+    cin>>b;
+    cout<<"Enter the third side : ";
+    cin>>c;
+    if(a+b>c || a+c>b || b+c>a){
+        cout<<"This is a triangle ";
+    }
+    else{
+        cout<<"This is not a triangle : ";
+    }
+}
