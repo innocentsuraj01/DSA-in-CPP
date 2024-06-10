50fc38877fa83b8f92f77e0504f1ab26f7f7b0b9
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-06-10 13:00:00 +0530

    added a new file

--- INPUT/grademarks.cpp
+++ INPUT/grademarks.cpp
@@ -7,34 +7,22 @@ int main(){
     if (n>=91){
         cout<<"Excellent"; 
     }
-    else{
-        if(n>=81){
-          cout<<"Very Good";  
-        }
-        else{
-            if(n>=71){
-            cout<<"Good";
-            }
-            else{
-                if(n>=61){
-                cout<<"Can do better";
-                }
-                else{
-                    if(n>=51){
-                        cout<<"Average";
-                    }
-                    else{
-                        if(n>=41){
-                            cout<<"Below Average";
-                        }
-                        else{
-                            if(n>40){
-                                cout<<"Fail";
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    else if(n>=81){
+    cout<<"Very Good";  
+    }
+    else if(n>=71){
+    cout<<"Good";
+    }
+    else if(n>=61){
+    cout<<"Can do better";
+    }
+    else if(n>=51){
+    cout<<"Average";
+    }
+    else if(n>=41){
+    cout<<"Below Average";
+    }
+    else {
+    cout<<"Fail";
     }
 }
