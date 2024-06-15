2529fb6338837174b28a3b379bd4d0bd6ee92554
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-06-15 11:03:07 +0530

    added a new file

---
+++ CONDATIONAL/countnotes.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Entern a number : ";
+    cin>>a;
+    switch (a){
+        case 1:
+        cout<<"note of " <<a " = "<<a;
+        case 2:
+        cout<<"note of " <<
+    }   
+}
