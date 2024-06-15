8010e19ed4e088d990370bca7ddfb52626435864
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-06-15 11:16:41 +0530

    added a new file

--- CONDATIONAL/countnotes.cpp
+++ CONDATIONAL/countnotes.cpp
@@ -2,12 +2,7 @@
 using namespace std;
 int main(){
     int a;
-    cout<<"Entern a number : ";
+    cout<<"Enter a number : ";
     cin>>a;
-    switch (a){
-        case 1:
-        cout<<"note of " <<a " = "<<a;
-        case 2:
-        cout<<"note of " <<
-    }   
+     
 }
--- CONDATIONAL/monthday.cpp
+++ CONDATIONAL/monthday.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int a;
-    cout<<"Entern a number : ";
+    cout<<"Enter a number : ";
     cin>>a;
     switch(a){
         case 1:
