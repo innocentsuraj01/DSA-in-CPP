225a19c6a4a3de97f9478af39aba8572e4246e52
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-09-05 17:37:38 +0530

    added a new file

--- CONDATIONAL/checktriangle.cpp
+++ CONDATIONAL/checktriangle.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
-using namespace std:
+using namespace std;
 int main()
 {
     int a,b,c;
-    cout<<""
+    cout<<"Enter First angle : ";
+    cin>>a;
+    cout<<"Enter Second angle : ";
+    cin>>b;
+    cout<<"Enter Third angle : ";
+    cin>>c;
+    if(a==b and b==c)
+    cout<<"Equilateral Triangle";
+    else if(a!=b and b!=c)
+    cout<<"Scalene Triangle";
+    else
+    cout<<"Isosceles Triangle";
+    
 } 
