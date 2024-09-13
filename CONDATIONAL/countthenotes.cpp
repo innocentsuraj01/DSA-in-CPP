13df742603c3d0a769756890ced6333a910eb231
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-09-13 22:41:18 +0530

    added a new file

---
+++ CONDATIONAL/countthenotes.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int amount;
+    int n1, n2, n5, n10, n20, n50, n100, n200, n500;
+    n1=n2=n5=n10=n20=n50=n100=n200=n500;
+
+    cout<<"Enter the amount : ";
+    cin>>amount;
+
+    switch (amount >= 500)
+    {
+    case 1:
+        
+        break;
+    }
+}
