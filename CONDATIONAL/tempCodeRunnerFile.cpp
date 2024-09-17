46ef515ed253ebbdd814e6729a84ce9b60a3d0ce
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-09-17 10:41:43 +0530

    added a new file

--- CONDATIONAL/tempCodeRunnerFile.cpp
+++ CONDATIONAL/tempCodeRunnerFile.cpp
@@ -1,11 +1,6 @@
-#include<iostream>
-// using namespace std;
-// int main() {
-//  int x = 3 ;
-//  float y = 3.0 ;
-//  if (x == y)
-//  cout <<"x and y are equal" ;
-//  else
-//  cout << "x and y are not equal" ;
-//  return 0;
-// }
+
+    }
+    else{
+        cout<<"circumference is grater or equal to the area ";
+    }
+    
