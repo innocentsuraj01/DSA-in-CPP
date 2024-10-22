f4ff1c63c631e26a2d267a56f686c33ef3b8a704
Innocent suraj01 <surajmaurya7561@gmail.com>
2024-10-22 22:18:23 +0530

    added a new file

---
+++ FUNCTIONAL/swap.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter first no. : ";
+    cin>>a;
+    cout<<"Enter second no. : ";
+    cin>>b;
+    int a = b;
+    int b = a;
+    cout<<a<<b<<endl;
+}
